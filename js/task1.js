39c89d7c81a7176a367c5ff19927eecef496b340
andriysoroka <dree0103@gmail.com>
2017-07-11 18:02:52 +0300

    forth

--- js/task1.js
+++ js/task1.js
@@ -3,9 +3,9 @@ var a = parseInt(prompt("write a", a)),
 	b = parseInt(prompt("write b", b)),
 	c = parseInt(prompt("write c", c)),
 	D = Math.pow(b, 2) - (4 * a * c),
-	result = D > 0 ? `x1 = ${res1 = (-b - Math.sqrt(D)) / (2 * a)}, x2 = ${res2 = (-b + Math.sqrt(D)) / (2 * a)}`
+	result = D > 0 ? `Discriminant = ${D} bigger than 0, so we have x1 = ${res1 = (-b - Math.sqrt(D)) / (2 * a)}, x2 = ${res2 = (-b + Math.sqrt(D)) / (2 * a)}`
 			: D < 0 ? `Discriminant = ${D} less than 0, so we do not have x1 and x2`
-			: `x =  ${- b / (2 * a)}`;
+			: `Discriminant = ${D} equal 0, so we have  x =  ${- b / (2 * a)}`;
 //check = (isNaN(D)) ? "You have not written numbers, please reload this page" : p; // can be to check if user write numbers
 //console.log(check);
 console.log(result);
