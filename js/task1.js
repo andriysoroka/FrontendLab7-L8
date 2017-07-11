c88eca6529507c4aaff488e4865430d4e1dd3215
andriysoroka <dree0103@gmail.com>
2017-07-11 17:47:01 +0300

    second

--- js/task1.js
+++ js/task1.js
@@ -1,16 +1,12 @@
-var a, b, c, res1, res2, D, less0, zero, x, res, result, nanka;
-zero = "zero";
-a = parseInt(prompt("write a", a));
-b = parseInt(prompt("write b", b));
-c = parseInt(prompt("write c", c));
-D = Math.pow(b, 2) - (4 * a * c);
-less0 = "Discriminant = " + D + " less than 0, so we don`t have x1 and x2";
-res2 = ((-b) + Math.sqrt(D)) / (2 * a);
-res1 = ((-b) - Math.sqrt(D)) / (2 * a);
-result = (D > 0) ? (`x1 = ${res1}, x2 = ${res2}`)
-			: (D < 0) ? less0
+//var a, b, c, res1, res2, D, less0, x, res, result, check;
+var a = parseInt(prompt("write a", a)),
+	b = parseInt(prompt("write b", b)),
+	c = parseInt(prompt("write c", c)),
+	D = Math.pow(b, 2) - (4 * a * c),
+	result = (D > 0) ? `x1 = ${res1 = (-b - Math.sqrt(D)) / (2 * a)}, x2 = ${res2 = (-b + Math.sqrt(D)) / (2 * a)}`
+			: (D < 0) ? `Discriminant = ${D} less than 0, so we do not have x1 and x2`
 			: (x = - 2 / 2 * a);
-//nanka = (isNaN(D)) ? "You have not written numbers, please reload this page" : p; // can be to check if user write numbers
-//console.log(nanka);
+//check = (isNaN(D)) ? "You have not written numbers, please reload this page" : p; // can be to check if user write numbers
+//console.log(check);
 console.log(result);
 
