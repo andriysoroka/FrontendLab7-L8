310bf9b1c0ff8b09f8be2f54b4ba5dfef5f2a476
andriysoroka <dree0103@gmail.com>
2017-07-11 19:01:03 +0300

    first

--- js/task2.js
+++ js/task2.js
@@ -1,6 +1,6 @@
 var dollar, euro, hryvnia, euroGR, dollarGR;
-euro = prompt("write euro", euro);
-dollar = prompt("write dollars", dollar);
-euroGR = parseInt(euro) * 29.75;
-dollarGR = parseInt(dollar) * 26.05;
+var euro = prompt("write euro", euro),
+	dollar = prompt("write dollars", dollar),
+	euroGR = parseInt(euro) * 29.75,
+	dollarGR = parseInt(dollar) * 26.05;
 alert(euro + " euros are equal " + euroGR + " UAH, " + dollar + " dollars are equal " + dollarGR + " UAH, one euro is equal " + 29.75/26.05 + " dollars.");
